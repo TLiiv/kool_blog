7e6b5f8bf3ae22856caeffcfae9ed8dee1c5e740
Tarmo <liivamagi.tarmo@gmail.com>
2024-04-27 19:46:07 +0300

    tund läbi

---
+++ resources/views/page1.blade.php
@@ -0,0 +1,5 @@
+@extends('partials.layout')
+@section('title','Page1')
+@section('content')
+    <h1>page1</h1>
+@endsection
---
+++ resources/views/page2.blade.php
@@ -0,0 +1,5 @@
+@extends('partials.layout')
+@section('title','Page2')
+@section('content')
+    <h1>page2</h1>
+@endsection
